0f549860b1d65ec56a0e93c447aa3b852cb38f02
annabrit <annabrit@gmail.com>
2017-12-15 15:24:49 -0800

    connected handler functions to routers

--- routers/users.js
+++ routers/users.js
@@ -1,26 +1,19 @@
 const express = require('express');
 
-const userHandler = //add this in
+const usersHandler = require('../handlers');
 
 const router = express.Router();
 
 router
 	.route('/users')
-		.get() //put function here
-		.post() //put function here
-
-router
-	.route('/users/new')
-		.get() //put function here
+		.get(usersHandler.getUsers) //put function here
+		.post(usersHandler.createUser) //put function here
 
 router
 	.route('/users/:id')
-		.get(//function)
-		.patch(//function from handler) 
-		.delete(//function from handler)
+		.get(usersHandler.getUser)
+		.patch(usersHandler.updateUser) 
+		.delete(usersHandler.deleteUser)
 
-router
-	.route('/users/:id/edit')
-		.get()
 
 module.exports = router;						
